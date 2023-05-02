5d40e6425b7bc27258e7002d406b78ee19dac508
audreyamarh <audreyamarh@penguin>
2023-05-02 11:15:16 +0000

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: 1 for uppercase character or 0 for anything else.
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
